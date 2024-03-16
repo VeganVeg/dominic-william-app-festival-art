7052beaa2d99f63ba7a21acef867944733021bbd
willRathier <willi02rm@gmail.com>
2024-03-15 20:51:42 -0400

    lp changes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-const likeButton = document.querySelector('.like-button');
-const heart = document.querySelector('.bi-heart-fill');
+const likeButton = document.querySelector(".like-button");
+const heart = document.querySelector(".bi-heart-fill");
 
-
-
-if (document.querySelector('.like-button') !== null) {
-
-    likeButton.addEventListener('click', function () {
-        likeButton.classList.add('liked-btn');
-        heart.classList.add('liked-heart');
-    });
+if (document.querySelector(".like-button") !== null) {
+  likeButton.addEventListener("click", function () {
+    likeButton.classList.add("liked-btn");
+    heart.classList.add("liked-heart");
+  });
 }
 
+const swiperFilters = new Swiper(".swiper-filters", {
+  freeMode: true,
+  slidesPerView: "auto",
+  spaceBetween: 10,
+});
 
-const swiperFilters = new Swiper('.swiper-filters', {
-    freeMode: true,
-    slidesPerView: 'auto',
-    spaceBetween: 10
-})
+const swiperArtworks = new Swiper(".swiper-featured-artworks", {
+  freeMode: true,
+  slidesPerView: "auto",
+  spaceBetween: 10,
+});
 
-
-const swiperArtworks = new Swiper('.swiper-featured-artworks', {
-    freeMode: true,
-    slidesPerView: 'auto',
-    spaceBetween: 10
-})
+const swiperPhone = new Swiper(".swiper-screenshots", {
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-befrore",
+  },
+});
