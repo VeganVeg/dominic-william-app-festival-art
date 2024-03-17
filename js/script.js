d863e8b1b612f1c159ae12b24d219de02f430cb5
willRathier <willi02rm@gmail.com>
2024-03-16 22:21:30 -0400

    will lp changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const likeButton = document.querySelector(".like-button");
 const heart = document.querySelector(".bi-heart-fill");
 
-
 if (document.querySelector(".like-button") !== null) {
   likeButton.addEventListener("click", function () {
     likeButton.classList.add("liked-btn");
@@ -10,6 +9,32 @@ if (document.querySelector(".like-button") !== null) {
 }
 
 const swiperPhone = new Swiper(".screenshots", {
+  loop: true,
+  effect: "coverflow",
+  centeredSlides: true,
+  slidesPerView: 3,
+  spaceBetween: 70,
+  coverflowEffect: {
+    rotate: 30,
+    stretch: 1,
+    depth: 50,
+    modifier: 1,
+    slideShadows: true,
+  },
+  /*loop: true,
+  effect: "shadow",
+  slidesPerView: 3,
+  spaceBetween: 10,
+  shadow: {
+    slidesShadow: true,
+  },
+   breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      slideShadows: true,
+    },
+  },*/
   navigation: {
     nextEl: ".btn-next",
     prevEl: ".btn-prev",
